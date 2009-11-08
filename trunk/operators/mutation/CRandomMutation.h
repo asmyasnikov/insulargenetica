a88552abd9be7cfb86398a37fc3228d78efd86eb
amyasnikov <amyasnikov@d366f6b8-1774-430b-8837-35b0147b9547>
2009-11-08 11:20:42 +0000

    Small changes of GNU Lesser notation in source files
    git-svn-id: https://svn.code.sf.net/p/insulargenetica/code@35 d366f6b8-1774-430b-8837-35b0147b9547

--- trunk/operators/mutation/CRandomMutation.h
+++ trunk/operators/mutation/CRandomMutation.h
@@ -1,28 +1,28 @@
 /****************************************************************
 Copyright (C) 2009 Aleksey Myasnikov
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser GPL as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser GPL as publishedby
+the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser GPL for more details.
-You should have received a copy of the GNU Lesser GPL
-in file LICENSE along with this program.  If not, see
-<http://www.gnu.org/licenses/>
+GNU Lesser GPL for more details. You should have received a
+copy of the GNU Lesser GPL in file LICENSE along with this
+program.  If not, see <http://www.gnu.org/licenses/lgpl.txt>
 I draw your attention to the fact that the library InsularGenetica
 is registered Russian Agency for Patents and Trademarks (ROSPATENT),
 which issued the "Certificate of official registration of computer
-programs for № FIXME FIXME FIXME FIXME". A copy of the certificate
+programs for N FIXME FIXME FIXME FIXME". A copy of the certificate
 of registration is presented in the file CERTIFICATE in the root
-of the project. It does not impose on the end developers / users,
-no additional restrictions other than those prescribed by GNU Lesser
+of the project. It does not impose on the end developers/users, no
+additional restrictions other than those prescribed by GNU Lesser
 GPL, while maintaining information about developer this library.
 ****************************************************************/
 /**
  * @file    CRandomMutation.h
  * @brief   Class CRandomMutation
+ * @author  Aleksey Myasnikov
  * @date    20/02/2009
  * @version 3.3
 **/
